fbf43d95902c4ffa593405e54446f73fd88f74b2
ISmeneger <ISmeneger@yandex.ru>
2025-04-21 19:00:43 +0300

    Add base test

--- src/test/java/pageFactoryTests/BaseTest.java
+++ src/test/java/pageFactoryTests/BaseTest.java
@@ -1,19 +1,24 @@
 package pageFactoryTests;
 
+import Ilya_S.configs.TestPropertiesConfig;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
 
+import static Ilya_S.patterns.factory.WebDriverFactory.createWebDriver;
+
 public class BaseTest {
-    WebDriver driver;
+    protected WebDriver driver;
+    TestPropertiesConfig configProperties = ConfigFactory.create(TestPropertiesConfig.class, System.getProperties());
+    protected static final String BASE_URL = "https://bonigarcia.dev/selenium-webdriver-java/";
 
     @BeforeEach
     void setup() {
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
+        driver = createWebDriver(configProperties.browser());
+        driver.get(BASE_URL);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
     }
 
